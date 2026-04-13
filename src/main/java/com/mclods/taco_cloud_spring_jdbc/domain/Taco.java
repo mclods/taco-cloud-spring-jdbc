8eb378bffa1afcc886f62e6fb565f756b925c48c
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-13 12:29:59 +0530

    Updated code to use Ingredient CrudRepository replace JdbcIngredientRepository (#2)
    Updated domain models to support spring data jdbc

--- src/main/java/com/mclods/taco_cloud_spring_jdbc/domain/Taco.java
+++ src/main/java/com/mclods/taco_cloud_spring_jdbc/domain/Taco.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,16 +15,21 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table("taco")
 public class Taco {
+    @Id
+    @Column("id")
     private int id;
 
     @NotNull
     @Size(min = 5, message = "Taco name must be at least 5 characters long")
+    @Column("name")
     private String name;
 
     @NotNull
     @Size(min = 1, message = "You must choose at least one ingredient")
     private List<Ingredient> ingredients;
 
+    @Column("creation_date")
     private LocalDateTime creationDate;
 }
